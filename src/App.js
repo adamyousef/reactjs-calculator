3ad81a9f16850ecc0d71b0fdc0cc012f0d98be5c
Wissem <wissem.jendoubi11@gmail.com>
2019-09-05 23:25:14 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -3,55 +3,53 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 class App extends React.Component {
-  constructor(){
-    super()
-    this.state={
-      result: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange = (button)=>{
-     if(button === "="){
-       this.retrunResult();
-     }
-     else if(button === "C"){
-       this.reset();
-     }
-     else if(button === "CE"){
-      var val = this.state.result;
-      val = val.slice(0, -1);
-      this.setState({ result :val } );
-     }
-     else{
-      var val = this.state.result + button;
-      this.setState({ result :val } );
-  }
-} 
-  retrunResult(){
-    try {
-      var val = eval(this.state.result);
-      this.setState({ result :val } );
+    constructor() {
+        super()
+        this.state = {
+            result: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
-    catch(err) {
-      alert("Your Input is incorrect ! Please try again !");
+    handleChange = (button) => {
+        if (button === "=") {
+            this.retrunResult();
+        } else if (button === "C") {
+            this.reset();
+        } else if (button === "CE") {
+            var val = this.state.result;
+            val = val.slice(0, -1);
+            this.setState({ result: val });
+        } else {
+            var val = this.state.result + button;
+            this.setState({ result: val });
+        }
     }
-  }
-  reset(){
-      var val = "";
-      this.setState({ result :val } );
-  }
- 
+    retrunResult() {
+        try {
+            var val = eval(this.state.result);
+            this.setState({ result: val });
+        } catch (err) {
+            var val = "NaN";
+            this.setState({ result: val });
+        }
+    }
+    reset() {
+        var val = "";
+        this.setState({ result: val });
+    }
 
-  render(){
-    return (
-      <div className="App">
-       <h1>My React Calculator</h1>
-       <Calculator result = {this.state.result}
-                   onClick={this.handleChange}
-       />
-      </div>
-    );
-  }
+
+    render() {
+        return ( <div className = "App" >
+            <h1> My React Calculator </h1>   <
+            Calculator result = { this.state.result }
+            onClick = { this.handleChange }
+            />   
+            </div >
+        );
+    }
+
+
 
 }
 
